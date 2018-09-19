c2077b81e6f2afe20201ccee7812c00c06ace9c1
Joaquin Volpe <joaquin.volpe@woopasoft.com>
2018-09-18 22:25:17 -0300

    Epa

---
+++ client/src/app/shared/components/track-activities/track-activities.component.ts
@@ -0,0 +1,23 @@
+// angular
+import { Component, Input, OnChanges } from '@angular/core';
+
+// models
+import {
+  TrackActivity
+} from '../../../shared/model/';
+
+@Component({
+  selector: 'racer-track-activities',
+  templateUrl: './track-activities.component.html',
+  styleUrls: ['./track-activities.component.scss']
+})
+export class TrackActivitiesComponent implements OnChanges {
+  @Input() trackActivities: TrackActivity[];
+  @Input() selectedTrackActivity: TrackActivity;
+
+  constructor() { }
+
+  ngOnChanges() {
+    console.log('AAA ' + this.trackActivities);
+  }
+}
---
+++ server/src/dummy/race/track-activities.ts
@@ -0,0 +1,128 @@
+// models
+import {
+  TrackActivity,
+  TrackActivityState
+} from './../../model/track-activity.model';
+
+export const TRACK_ACTIVITIES_SAMPLE: TrackActivity[] = [
+  {
+    name: 'Comunitarias 1 grupo "B"',
+    short_name: 'COM "B"',
+    date: '2018-10-12T00:10:50.000Z',
+    duration: 20,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Comunitarias 1 grupo "A"',
+    short_name: 'COM "A"',
+    date: '2018-10-12T00:11:15.000Z',
+    duration: 20,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Comunitarias 2 grupo "B"',
+    short_name: 'COM "B"',
+    date: '2018-10-12T00:13:35.000Z',
+    duration: 20,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Comunitarias 2 grupo "A',
+    short_name: 'COM "A"',
+    date: '2018-10-12T00:14:00.000Z',
+    duration: 20,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Comunitarias 3 grupo "B"',
+    short_name: 'COM "B"',
+    date: '2018-10-12T00:16:25.000Z',
+    duration: 20,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Comunitarias 3 grupo "A',
+    short_name: 'COM "A"',
+    date: '2018-10-12T00:16:50.000Z',
+    duration: 20,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Entrenamiento 1 grupo "B"',
+    short_name: 'E1 "B"',
+    date: '2018-10-13T00:09:15.000Z',
+    duration: 20,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Entrenamiento 1 grupo "A"',
+    short_name: 'E1 "A"',
+    date: '2018-10-13T00:09:40.000Z',
+    duration: 20,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Clasificacion 1 grupo "B"',
+    short_name: 'C1 "B"',
+    date: '2018-10-13T00:11:50.000Z',
+    duration: 10,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Clasificacion 1 grupo "A"',
+    short_name: 'C1 "A"',
+    date: '2018-10-13T00:11:55.000Z',
+    duration: 10,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Clasificacion 2 grupo "B"',
+    short_name: 'C2 "B"',
+    date: '2018-10-13T00:13:10.000Z',
+    duration: 10,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Clasificacion 2 grupo "A"',
+    short_name: 'C2 "A"',
+    date: '2018-10-13T00:13:25.000Z',
+    duration: 10,
+    laps: 0,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: '1º Serie',
+    short_name: 'SERIE1',
+    date: '2018-10-13T00:18:20.000Z',
+    duration: 0,
+    laps: 6,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: '2º Serie',
+    short_name: 'SERIE2',
+    date: '2018-10-13T00:18:50.000Z',
+    duration: 0,
+    laps: 6,
+    state: TrackActivityState.waiting
+  },
+  {
+    name: 'Final',
+    short_name: 'FINAL',
+    date: '2018-10-14T00:14:35.000Z',
+    duration: 0,
+    laps: 6,
+    state: TrackActivityState.waiting
+  }
+];
