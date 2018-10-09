b83af6ff4cb97b3eec6b9c352749d0e758b148fa
Joaquin Volpe <joaquin.volpe@woopasoft.com>
2018-10-09 01:03:13 -0300

    MAGIC

--- client/src/app/timing/containers/timing-home/secondary-bar/timing-secondary-bar.component.ts
+++ client/src/app/timing/containers/timing-home/secondary-bar/timing-secondary-bar.component.ts
@@ -25,15 +25,15 @@ export class TimingSecondaryBarComponent implements OnInit {
   isExpanded = false;
   isUIExpanded = true;
 
-  constructor(private store: Store<fromTiming.State>) {
+  constructor(private _store: Store<fromTiming.State>) {
   }
 
   ngOnInit(): void {
-    this.store.pipe(select(fromTiming.getRaceParticipantsTrackActivitiesArray)).subscribe(
-      (raceParticipantsTrackActivities: RaceParticipantTrackActivity[]) => {
-      if (raceParticipantsTrackActivities && raceParticipantsTrackActivities.length > 0) {
-        this.isExpanded = false;
-      }
-    });
+    // this.store.pipe(select(fromTiming.getRaceParticipantsTrackActivitiesArray)).subscribe(
+    //   (raceParticipantsTrackActivities: RaceParticipantTrackActivity[]) => {
+    //   if (raceParticipantsTrackActivities && raceParticipantsTrackActivities.length > 0) {
+    //     this.isExpanded = false;
+    //   }
+    // });
   }
 }
